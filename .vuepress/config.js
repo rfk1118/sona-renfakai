18d2b681efdd9a5338183dd31c6ca1ca47d7861c
renfakai <renfakai@58.com>
2021-05-18 18:46:59 +0800

    feat:init

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "sona",
+  description: "sona's Blog",
+  themeConfig: {
+    nav: [
+      {text: "首页", link: "/"},
+      {text: "指南", link: "/docs/"},
+    ],
+  },
+};
