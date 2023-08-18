c962efa831b275d5e71cdcfcbb238dc3d0ae457a
rfk1118 <396436290@qq.com>
2023-08-18 15:32:54 +0000

    Deploying to gh-pages from @ rfk1118/sona-renfakai@d572c917eb0fcbdfe3aa79b3f5fb04f5e9f14a00 🚀

---
+++ assets/index.html-341460db.js
@@ -0,0 +1,20 @@
+import{_ as r,V as d,W as s,X as n,Y as e,Z as t,$ as l,a0 as o,a1 as a,F as h}from"./framework-e54e0297.js";const c="/assets/WX20230225-100811@2x-20d55c21.png",u={},_=e("h1",{id:"",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#","aria-hidden":"true"},"#")],-1),f=e("div",{class:"hint-container tip"},[e("p",{class:"hint-container-title"},"悟"),e("pre",null,[e("code",null,`                                            悟
+                                        悟道休言天命，
+                                        修行勿取真经。
+                                        一悲一喜一枯荣，
+                                        哪个前生注定？
+                                        袈裟本无清净，
+                                        红尘不染性空。
+                                        幽幽古刹千年钟，
+                                        都是痴人说梦。
+`)])],-1),p=e("p",null,"无，名天地之始，有，名万物之母，故常无，欲以观其妙；常有，欲以观其徼。悟道别人给不了，可参考《遥远救世主》芮小丹对白。",-1),B=o('<h2 id="个人信息" tabindex="-1"><a class="header-anchor" href="#个人信息" aria-hidden="true">#</a> 个人信息</h2><p>任发凯 ｜ 山东建筑大学 ｜ 8 年工作经验 ｜ 15506466566 ｜ <a href="mailto:rfk1118@gmail.com">rfk1118@gmail.com</a></p><h2 id="商业意向" tabindex="-1"><a class="header-anchor" href="#商业意向" aria-hidden="true">#</a> 商业意向</h2><ul><li>无求职意向</li><li>web3科学家，有需求微信私聊。</li></ul>',4),m=e("h2",{id:"工作经历",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#工作经历","aria-hidden":"true"},"#"),a(" 工作经历")],-1),x=e("li",null,"赏金猎人，web3初级科学家 （2022/05 ～ 至今）",-1),E=e("h2",{id:"技能清单",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#技能清单","aria-hidden":"true"},"#"),a(" 技能清单")],-1),g=e("ul",null,[e("li",null,"什么都不会")],-1),b=o('<h2 id="为什么写博客" tabindex="-1"><a class="header-anchor" href="#为什么写博客" aria-hidden="true">#</a> 为什么写博客？</h2><ol><li>工作多年发现能清晰、准确、简单的表达想法是一件很难的事，写作恰巧能锻炼这项能力，并且这种方式很高效，拥有这项能力可以让沟通、阅读成本更低；</li><li>学习技术看概念三天就忘了，在写作期间发现了适合自己的学习方案（读源码、找理论知识、写博客、讲解），坚持很久后发现学技术变的简单了；</li><li>记录工作、读书中思考的点点滴滴，随手写作进行记录。</li></ol><h2 id="成长" tabindex="-1"><a class="header-anchor" href="#成长" aria-hidden="true">#</a> 成长</h2><ol><li>从入行开发就使用Spring，然后是SpringBoot，但是让我成长最快的是写了一年scf框架，因为scf需要自己手动管理资源。</li><li>未来一段时间写写C++，体会手动管理内存以深刻理解垃圾回收器。</li></ol><h2 id="关于博客" tabindex="-1"><a class="header-anchor" href="#关于博客" aria-hidden="true">#</a> 关于博客</h2><p>每次写新东西都会发现之前文章表述不清，需要修正（修正时间排期中...）。</p><ol><li>如果影响到了您的阅读并让您产生了思维误区，敬请见谅，也可通过微信反馈。</li><li>如果文章中推荐的工具效率不高，希望您可以通过微信联系反馈，3ks。</li><li>此博客开始放飞自我，股票，历史，哲学。</li></ol><div align="center"><img src="'+c+'" width="300" height="300" style="margin:0 auto;"></div>',8);function A(v,k){const i=h("RouterLink");return d(),s("div",null,[_,f,p,n(" 喜欢技术、金融的 `it` 人，在技术上对`领域、设计模式`喜欢更多一些，每设计出一个`稳定、易扩展`系统时生命价值感都会得到满足。 "),B,n(" Java 资深开发工程师 ｜ 全职 ｜ 北京/廊坊 ｜ 薪资面议 ｜ 到岗时间面议 "),m,e("ol",null,[x,e("li",null,[t(i,{to:"/introduction/#%E4%B8%AA%E4%BA%BA%E7%BB%8F%E5%8E%86"},{default:l(()=>[a("58集团 （2020/05 ～ 2022/04）")]),_:1})]),e("li",null,[t(i,{to:"/introduction/#%E4%B8%AA%E4%BA%BA%E7%BB%8F%E5%8E%86"},{default:l(()=>[a("沣邦融资租赁（上海）有限公司 （2018/07 ～ 2020/05）")]),_:1})]),e("li",null,[t(i,{to:"/introduction/#%E4%B8%AA%E4%BA%BA%E7%BB%8F%E5%8E%86"},{default:l(()=>[a("逸途（北京）科技有限公司 （2015/08 ～ 2018/07）")]),_:1})])]),E,g,n(` * **开发语言：** 熟练 Java、了解 Golang、汇编
+* **后端框架：** 熟练 Spring、SpringMvc、Mybatis、SpringBoot、熟悉 SpringData、Struts2、Hibernate 了解 SpringCloud
+* **数据库：** 熟练 Oracle、Mysql 熟悉 Redis
+* **中间件：** 熟悉 Netty、 Kafka 了解 Zookeeper
+* **服务器：** 熟悉 Jetty、Tomcat
+* **构建工具：** 熟悉 Maven
+* **项目管理工具：** 熟悉 Git
+* **脚本语言：** 熟悉 Shell
+* **设计模式：** 熟练 23 种设计模式
+* **网络：** 了解 Wireshark
+* **工作流：** 熟悉 Activiti、Flowable `),b])}const N=r(u,[["render",A],["__file","index.html.vue"]]);export{N as default};
